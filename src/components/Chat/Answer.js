5378574bd107b8e42256e2d9ae380ec2a302d472
Bharat Soni <bharatchills@gmail.com>
2023-01-25 23:25:11 +0530

    added svg animation in answer-box

--- src/components/Chat/Answer.js
+++ src/components/Chat/Answer.js
@@ -1,9 +1,85 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
 
 export default function Answer({ data }) {
+  const createSVG = (width, height, radius) => {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+    const rectangle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "rect"
+    );
+
+    svg.setAttributeNS(
+      "http://www.w3.org/2000/svg",
+      "viewBox",
+      `0 0 ${width} ${height}`
+    );
+
+    rectangle.setAttribute("x", "0");
+    rectangle.setAttribute("y", "0");
+    rectangle.setAttribute("width", "100%");
+    rectangle.setAttribute("height", "100%");
+    rectangle.setAttribute("rx", `${radius}`);
+    rectangle.setAttribute("ry", `${radius}`);
+    rectangle.setAttribute("pathLength", "10");
+
+    svg.appendChild(rectangle);
+
+    return svg;
+  };
+  const [isHover, setIsHover] = useState(false);
+  const buttonRef = useRef(null);
+
+  function onMouseEnterHandle() {
+    setIsHover(true);
+    console.log(isHover)
+  }
+
+  function onMouseLeaveHandle() {
+    setIsHover(false);
+  }
+
+  function svgAnimation(){
+    console.log("svg")
+    const button = buttonRef.current
+    const style = getComputedStyle(button);
+    const lines = document.createElement("div");
+    lines.classList.add("lines");
+    const groupTop = document.createElement("div");
+    const groupBottom = document.createElement("div");
+    const svg = createSVG(button.offsetWidth, button.offsetHeight, parseInt(style.borderRadius, 20));
+
+    groupTop.appendChild(svg);
+    groupTop.appendChild(svg.cloneNode(true));
+    groupTop.appendChild(svg.cloneNode(true));
+    groupTop.appendChild(svg.cloneNode(true));
+
+    groupBottom.appendChild(svg.cloneNode(true));
+    groupBottom.appendChild(svg.cloneNode(true));
+    groupBottom.appendChild(svg.cloneNode(true));
+    groupBottom.appendChild(svg.cloneNode(true));
+
+    lines.appendChild(groupTop);
+    lines.appendChild(groupBottom);
+
+    button.appendChild(lines);
+
+  }
+
+  useEffect(() => {
+    console.log("ds")
+    svgAnimation()
+  }, []);
   return (
-    <Link className="Answer" to={data.link}>
+    <Link
+    ref={buttonRef}
+      onMouseEnter={onMouseEnterHandle}
+      onMouseLeave={onMouseLeaveHandle}
+      className={isHover?"Answer start animated-box":"Answer animated-box"}
+      to={data.link + isHover}
+    >
       {data.label}
     </Link>
   );
